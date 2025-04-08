ebb519fd2781fa1f9f77f47daa7d17a52b179e5c
Saberhui1111 <ilj4ger@yandex.ru>
2025-04-09 02:07:13 +0300

    upd

--- BlackBoxOptimizer/BoxOptimizer/MADS/MADSOptimizer.py
+++ BlackBoxOptimizer/BoxOptimizer/MADS/MADSOptimizer.py
@@ -1,13 +1,8 @@
 import PyNomad
 import numpy as np
 from typing import Callable
-import os
-import sys
 
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../'))
-if project_root not in sys.path:
-    sys.path.insert(0, project_root)
-
+from ..BaseOptimizer import BaseOptimizer
 
 from BlackBoxOptimizer.BoxOptimizer.BaseOptimizer import BaseOptimizer, OptimizedVectorData
 
