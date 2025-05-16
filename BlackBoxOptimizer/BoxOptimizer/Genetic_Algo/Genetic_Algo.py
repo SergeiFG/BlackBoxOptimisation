582975fda61ed3f67223b338d5441568e2add002
Qwazikpotik <brsbelaev@gmail.com>
2025-05-17 02:20:28 +0300

    Added bool param

--- BlackBoxOptimizer/BoxOptimizer/Genetic_Algo/Genetic_Algo.py
+++ BlackBoxOptimizer/BoxOptimizer/Genetic_Algo/Genetic_Algo.py
@@ -1,4 +1,4 @@
-from typing import Callable, Optional, Tuple
+from typing import Callable, Optional, List
 import numpy as np
 from ..BaseOptimizer import BaseOptimizer
 from .OptClass import GeneticAlgorithmOptimizer
@@ -9,8 +9,15 @@ class Genetic_Algo(BaseOptimizer):
                  min_mutation: float = 0.2, elite_size: int = 5,
                  output_lower_bounds: Optional[np.ndarray] = None,
                  output_upper_bounds: Optional[np.ndarray] = None,
+                 discrete_indices: Optional[List[int]] = None,
                  *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        base_kwargs = {
+            'to_model_vec_size': kwargs.pop('to_model_vec_size', dimension),
+            'from_model_vec_size': kwargs.pop('from_model_vec_size', 1),
+            'iter_limit': kwargs.pop('iter_limit', generations),
+            'seed': seed
+        }
+        super().__init__(**base_kwargs)
         self.seed = seed
         self.dimension = dimension
         self.population_size = population_size
@@ -20,41 +27,25 @@ class Genetic_Algo(BaseOptimizer):
         self.elite_size = elite_size
         self.output_lower_bounds = output_lower_bounds
         self.output_upper_bounds = output_upper_bounds
+        self.discrete_indices = discrete_indices if discrete_indices is not None else []
         self.best_output_values = None
 
     def configure(self, **kwargs):
         super().configure(**kwargs)
-        if 'dimension' in kwargs:
-            self.dimension = kwargs['dimension']
-        if 'output_lower_bounds' in kwargs:
-            self.output_lower_bounds = kwargs['output_lower_bounds']
-        if 'output_upper_bounds' in kwargs:
-            self.output_upper_bounds = kwargs['output_upper_bounds']
+        for key in ['dimension', 'output_lower_bounds', 'output_upper_bounds', 'discrete_indices']:
+            if key in kwargs:
+                setattr(self, key, kwargs[key])
 
-    def _get_bounds(self) -> Tuple[np.ndarray, np.ndarray]:
-        """Получаем ограничения из BaseOptimizer"""
-        lower = np.full(self._to_model_vec_size, -np.inf)
-        upper = np.full(self._to_model_vec_size, np.inf)
-        
-        for i in range(self._to_model_vec_size):
-            lower[i] = self._to_opt_model_data._vec[i, 0]  # min_index
-            upper[i] = self._to_opt_model_data._vec[i, 1]  # max_index
-            
+    def _get_bounds(self):
+        """Получение границ из BaseOptimizer"""
+        lower = np.array([prop.min for prop in self._to_opt_model_data._values_properties_list])
+        upper = np.array([prop.max for prop in self._to_opt_model_data._values_properties_list])
         return lower, upper
 
     def modelOptimize(self, func: Callable[[np.ndarray], np.ndarray]) -> None:
-        # Получаем ограничения из BaseOptimizer
         lower_bounds, upper_bounds = self._get_bounds()
-        
-        # Создаем обертку для функции, чтобы обрабатывать несколько выходов
-        def wrapped_func(x: np.ndarray) -> np.ndarray:
-            result = func(x)
-            if not isinstance(result, np.ndarray):
-                result = np.array([result])
-            return result
-        
         ga = GeneticAlgorithmOptimizer(
-            func=wrapped_func,
+            func=func,
             dimension=self._to_model_vec_size,
             population_size=self.population_size,
             generations=self.generations,
@@ -64,19 +55,14 @@ class Genetic_Algo(BaseOptimizer):
             lower_bounds=lower_bounds,
             upper_bounds=upper_bounds,
             output_lower_bounds=self.output_lower_bounds,
-            output_upper_bounds=self.output_upper_bounds
+            output_upper_bounds=self.output_upper_bounds,
+            discrete_indices=self.discrete_indices
         )
-        
-        # Запускаем оптимизацию (теперь возвращает три значения)
         best_x, best_f, best_outputs = ga.run()
-        self.best_output_values = best_outputs
-
-        # Записываем best_x в контейнер BaseOptimizer
         for to_vec in self._to_opt_model_data.iterVectors():
             to_vec[:] = best_x
+        self.best_output_values = best_outputs
 
     def getResult(self) -> np.ndarray:
         historical_data = self.getHistoricalData("vec_to_model")
-        if historical_data and len(historical_data) > 0:
-            return historical_data[-1]
-        return np.array([])
+        return historical_data[-1] if historical_data else np.array([])
